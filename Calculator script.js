ba86025fe2f0196df99c05355ff559b49d6ce20a
WolfAufovich <aufovichwolf@gmail.com>
2025-05-16 10:17:54 +0300

    Update Calculator script.js

--- Calculator script.js
+++ Calculator script.js
@@ -18,15 +18,21 @@ for (i = 0; i < Buttons.length; i++){
 		}
 		else if (this.innerHTML == "Clear"){
 			Value.innerHTML = '';
+			open_flag=0;
+			tochka_flag=1;
 		}
 		else if(this.innerHTML=='b'){
     if(scobki.includes(Value.innerHTML.charAt(Value.innerHTML.length - 1))){
       open_flag=(open_flag == 1) ? 0 : 1;
 }
-			Value.innerHTML = Value.innerHTML.slice(0, -1);
+			//Value.innerHTML = Value.innerHTML.slice(0, -1);
    
-		}
-		else if ((Value.innerHTML == '' || znaki.includes(Value.innerHTML[Value.innerHTML.length - 1]) || Value.innerHTML[Value.innerHTML.length - 1]=='(') && (this.innerHTML == '/' || this.innerHTML == '*' || this.innerHTML == '+' || this.innerHTML == '-' || this.innerHTML == '.')){
+		//}
+		else if(this.innerHTML == '.' && tochka_flag==1 && zzifry.includes(Value.innerHTML[Value.innerHTML.length - 1])){ // точка
+			Value.innerHTML += this.innerHTML;
+			tochka_flag=0;
+		} 						
+		else if ((Value.innerHTML == '' || znaki.includes(Value.innerHTML[Value.innerHTML.length - 1]) || Value.innerHTML[Value.innerHTML.length - 1]=='(') && (this.innerHTML == '/' || this.innerHTML == '*' || this.innerHTML == '+' || this.innerHTML == '-' || (this.innerHTML == '.' && tochka_flag==0))){
 			Value.innerHTML +='';
 		}
 		else if( this.innerHTML=='(' && open_flag==0 && (znaki.includes(Value.innerHTML[Value.innerHTML.length - 1]) || Value.innerHTML=='')){
@@ -37,8 +43,11 @@ for (i = 0; i < Buttons.length; i++){
 			Value.innerHTML += this.innerHTML;
 			open_flag = 0;
 		}
-		else if (this.innerHTML != '=' && !scobki.includes(this.innerHTML)){
+		else if (this.innerHTML != '=' && !scobki.includes(this.innerHTML) && this.innerHTML != '.'){
 			Value.innerHTML += this.innerHTML;
+			if(znaki.includes(this.innerHTML)){
+				tochka_flag=1;
+			}
 		}
 	})
 }
